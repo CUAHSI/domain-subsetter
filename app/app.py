2f190daa4c3f258d35ee9c173fcf1882b5efe2ef
Castronova <castronova.anthony@gmail.com>
2018-08-22 16:07:54 -0400

    added basic falcon app

---
+++ app/app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+import falcon
+
+class QuoteResource:
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        quote = {
+            'quote': (
+                "I've always been more interested in "
+                "the future than in the past."
+            ),
+            'author': 'Grace Hopper'
+        }
+
+        resp.media = quote
+
+api = falcon.API()
+api.add_route('/quote', QuoteResource())
