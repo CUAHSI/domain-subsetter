f72d664bd88b0e880a9fefcf388694f9771e3a87
Castronova <castronova.anthony@gmail.com>
2019-01-07 17:08:20 -0500

    added more logging options

--- tornado-app/environment.py
+++ tornado-app/environment.py
@@ -12,6 +12,10 @@ template_path = os.path.join(os.path.dirname(__file__), "templates")
 
 # logging settings
 
+log_dir = os.path.join(os.getcwd(), 'logs')
+log_file_size = 1024 * 20
+log_count = 10
+
 # levels: DEBUG, INFO, WARNING, ERROR, CRITICAL
 access_level = 'INFO'
 application_level = 'INFO'
--- tornado-app/logs.py
+++ tornado-app/logs.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import logging
 import environment as env
@@ -10,12 +11,21 @@ from tornado.log import app_log, gen_log, access_log, LogFormatter
 
 class Logs(object):
 
+    if not os.path.exists(env.log_dir):
+        os.makedirs(env.log_dir)
+
     # prepare logs for tornado general
     format = 'GENERAL: %(asctime)-15s %(message)s'
     formatter = LogFormatter(fmt=format, color=True)
     general_logger = logging.getLogger('tornado.general')
     general_logger.setLevel(env.general_level)
-    general_handler = logging.StreamHandler(sys.stdout)
+    general_logger.propagate = False
+
+    fpath = os.path.join(env.log_dir, 'general.log')
+    general_handler = logging.handlers.RotatingFileHandler(fpath,
+                                                           'a',
+                                                          env.log_file_size,
+                                                          env.log_count)
     general_handler.setFormatter(formatter)
     general_logger.addHandler(general_handler)
 
@@ -24,7 +34,13 @@ class Logs(object):
     access_formatter = LogFormatter(fmt=format, color=True)
     access_logger = logging.getLogger('tornado.access')
     access_logger.setLevel(env.access_level)
-    access_handler = logging.StreamHandler(sys.stdout)
+    access_logger.propagate = False
+
+    fpath = os.path.join(env.log_dir, 'access.log')
+    access_handler = logging.handlers.RotatingFileHandler(fpath,
+                                                          'a',
+                                                          env.log_file_size,
+                                                          env.log_count)
     access_handler.setFormatter(access_formatter)
     access_logger.addHandler(access_handler)
 
@@ -33,6 +49,12 @@ class Logs(object):
     application_formatter = LogFormatter(fmt=format, color=True)
     application_logger = logging.getLogger('tornado.application')
     application_logger.setLevel(env.application_level)
-    application_handler = logging.StreamHandler(sys.stdout)
+    application_logger.propagate = False
+
+    fpath = os.path.join(env.log_dir, 'application.log')
+    application_handler = logging.handlers.RotatingFileHandler(fpath,
+                                                               'a',
+                                                          env.log_file_size,
+                                                          env.log_count)
     application_handler.setFormatter(application_formatter)
     application_logger.addHandler(application_handler)
