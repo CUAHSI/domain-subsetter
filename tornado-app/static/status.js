b6b79c99a184b7666339b2c505b6e66fa8194677
Castronova <castronova.anthony@gmail.com>
2019-01-15 12:54:06 -0500

    added missing files

---
+++ tornado-app/static/status.js
@@ -0,0 +1,58 @@
+
+$(document).ready(function() {
+
+    // get the guid, necessary to poll job status
+    var guid = window.location.href.split('/').pop();
+
+    // initial request so there isn't a polling delay 
+	get_job_status(guid);
+
+	// begin polling every 20000 ms
+    pollInterval = setInterval(function(){
+	    get_job_status(guid);
+
+	}, 20000);
+
+});
+
+
+function get_job_status(guid) {
+    $.ajax({url: "/jobs/"+guid,
+	success: function(result) {
+	    res = JSON.parse(result);
+
+        if (res.status == 'finished') {
+
+            // hide the progress image
+            $('div.progress')[0].style.visibility = 'hidden';
+            
+            // print success message
+            $('#status')[0].innerHTML = 'Job Complete';
+            
+            clearInterval(pollInterval);
+
+            // display file info
+            $('#results')[0].style.display = '';
+            $('#file')[0].innerHTML = res.file;
+            $('#file')[0].setAttribute('href', res.file);
+
+        } else if (res.status == 'error') {
+            
+            // hide the progress image
+            $('div.progress')[0].style.visibility = 'hidden';
+
+            // display error message
+            $('#status')[0].innerHTML = 'The specified file could not be found :(';
+
+            clearInterval(pollInterval);
+        }
+        else {
+            // show the progress image
+            $('div.progress')[0].style.visibility = 'visible';
+        }
+    },
+    error: function(error) {
+            console.log('error');
+    }
+    });
+}
