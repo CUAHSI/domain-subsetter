f6bd6dcf3a3b800c8f447c44dfeab3cd9c14efb8
Castronova <castronova.anthony@gmail.com>
2019-01-17 10:55:22 -0500

    cleaning log messages

--- tornado-app/jobs.py
+++ tornado-app/jobs.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 
-
 import uuid
 import sqldata
+import  multiprocessing as mp
 from datetime import datetime
-from multiprocessing import Process, Queue
-from tornado.log import app_log, gen_log, access_log
+from tornado.log import app_log
+from multiprocessing_logging import install_mp_handler
+
 
 class BackgroundWorker(object):
 
     def __init__(self):
         self.numprocesses = 4
 
-        self.queue = Queue()
+        self.queue = mp.Queue()
         self.jobs = {}
 
         self.sql = sqldata.Connect()
@@ -20,9 +21,12 @@ class BackgroundWorker(object):
 
         self.Processes = []
 
+        # initialize the multiprocessing logger
+        install_mp_handler(logger=app_log)
+
         # starting workers
         for n in range(self.numprocesses):
-            process = Process(target=self.worker)
+            process = mp.Process(target=self.worker, args=(app_log,))
             process.start()
             self.Processes.append(process)
 
@@ -37,21 +41,26 @@ class BackgroundWorker(object):
         self.jobs[uid] = item
 
         app_log.info('job queued: %s' % uid) 
+
         self.sql.save_job(uid, item['state'], '')
 
         return uid       
 
     
-    def worker(self):
+    def worker(self, logger):
         while True:
+            logger.info('ready and waiting for job') 
+
             # get queued item 
             item = self.queue.get()
             
-            app_log.info('begin job: %s' % item['uid']) 
-
             try:
                 uid = item['uid']
+                
+                # save the start time for reporting
                 start = datetime.now()
+                item['dt_start'] = start
+
                 self.sql.save_job(uid,
                                   'running',
                                   '',
@@ -62,42 +71,39 @@ class BackgroundWorker(object):
                 self.jobs[uid] = item
                 
                 # run the job
-                res = item['function'](*item['args'],**item['kwargs'])
+                res = item['function'](*item['args'],**item['kwargs'],
+                                       logger=logger)
             
-                app_log.info('completed job: %s' % item['uid']) 
+                logger.info('job completed %s' % (item['uid']))
                 
                 # save output and yield result
                 item['dt_end'] = datetime.now()
-                item['dt_start'] = start
                 item['result'] = res
                 item['state'] = 'finished'
                 self.jobs[uid] = item
                 
+                
             except Exception as e:
-                print('job failed: %s' % e)
+                logger.error('job failed %s: %s' % (item['uid'], e))
                 item['state'] = 'failed'
                 item['result'] = dict(filepath='')
+                item['dt_end'] = datetime.now()
                 self.jobs[uid] = item
-                app_log.error('%s: %s' % (item['uid'], e))
+                
 
             try:
+                logger.info(item)
                 self.sql.save_job(uid,
                               item['state'],
                               item['result']['filepath'],
                               item['dt_start'],
                               item['dt_end'])
-                app_log.info('saved job results to db: %s' % item['uid'])
+                logger.info('results saved to db %s' % (item['uid']))
             except:
-                app_log.error('%s: could not find output file' % item['uid'])
+                logger.error('could not find output file %s' % 
+                             (item['uid']))
                 self.sql.save_job(uid,
                               item['state'],
                               '',
                               item['dt_start'],
                               item['dt_end'])
-
-
-
-
-
-
-
--- tornado-app/subset.py
+++ tornado-app/subset.py
@@ -7,12 +7,14 @@ import subprocess
 import transform
 import tarfile
 import shutil
-
 import environment as env
-from tornado.log import app_log, gen_log, access_log, LogFormatter
 
 
-def subset_nwm_122(uid, ymin, xmin, ymax, xmax):
+def subset_nwm_122(uid, ymin, xmin, ymax, xmax, logger=None):
+    
+    if logger is None:
+        from tornado.log import app_log
+        logger = app_log
 
     # list object to store stdout info for debugging
     stdout = []
@@ -30,15 +32,15 @@ def subset_nwm_122(uid, ymin, xmin, ymax, xmax):
             float(ymax))
 
 #    # check that bbox is valid
-#    app_log.info('validating bounding box')
+#    logger.info('validating bounding box')
 #    if (ymin > ymax) | (xmin > xmax):
-#        app_log.error('invalid bounding box')
+#        logger.error('invalid bounding box')
 #        return
 
     # TODO: check bbox size
 
     # run R script and save output as random guid
-    app_log.info('begin subsetting algorithm: %s' % uid )
+    logger.info('begin subsetting %s' % (uid) )
 
     cmd = ['Rscript',
            'subset_domain.R',
@@ -66,20 +68,19 @@ def subset_nwm_122(uid, ymin, xmin, ymax, xmax):
     if not return_code == 0:
         msg= 'The process call "{}" returned with code {}, an error ' \
              'occurred.'.format(list(cmd), return_code)
-        app_log.error('subsetting failed %s: %s' % (uid, msg) )
+        logger.error('subsetting failed %s: %s' % (uid, msg) )
         response = dict(message=msg, status='error')
         return response
 
-    app_log.info('subsetting succeeded %s' % uid )
+    logger.info('subsetting complete %s' % (uid) )
 
-    app_log.info('begin compressing results %s' % uid) 
     # compress the results
     fpath = os.path.join('/tmp', uid)
     outname = '%s.tar.gz' % uid
     with tarfile.open('/tmp/'+outname,  "w:gz") as tar:
         tar.add(fpath, arcname=os.path.basename(fpath))
     shutil.rmtree(fpath)
-    app_log.info('finished compressing results %s' % uid) 
+    logger.info('finished compressing results %s' % (uid)) 
 
     response = dict(message='file created at: /tmp/data/%s' % outname,
                         filepath='/data/%s' % outname,
@@ -88,95 +89,95 @@ def subset_nwm_122(uid, ymin, xmin, ymax, xmax):
 
 
 
-def subset_by_bbox(uid, llat, llon, ulat, ulon):
-
-    # list object to store stdout info for debugging
-    stdout = []
-    stdout.append('UID: %s\n'
-                  'llat (WGS84): %s\n'
-                  'llon (WGS84): %s\n'
-                  'ulat (WGS84): %s\n'
-                  'ulon (WGS84): %s\n\n' %(uid, str(llat), str(llon), str(ulat), str(ulon)))
-
-    geofile = '/home/acastronova/www.nco.ncep.noaa.gov/pmb/codes/nwprod/nwm.v1.2.2/parm/domain/geo_em.d01_1km.nc'
-    bbox = (float(llon),
-            float(llat),
-            float(ulon),
-            float(ulat))
-    coords = [(bbox[0], bbox[1]), # lower left
-              (bbox[2], bbox[3]), # upper left
-              (bbox[2], bbox[3]), # upper right
-              (bbox[0], bbox[1])] # lower right
-    transformed = transform.proj_to_coord(coords)
-    xs = [t[0] for t in transformed]
-    ys = [t[1] for t in transformed]
-    ysouth= min(ys)
-    ynorth= max(ys)
-    xwest = min(xs)
-    xeast=  max(xs)
-    stdout.append('Transformed (WGS -> Lambert Conformal Conic Variant)\n'
-                  'llat (LCC, m): %s\n'
-                  'llon (LCC, m): %s\n'
-                  'ulat (LCC, m): %s\n'
-                  'ulon (LCC, m): %s\n\n' %(str(ysouth), str(xwest), str(ynorth), str(xeast)))
-
-    # check that bbox is valid
-    print('validating bounding box')
-    if (ysouth > ynorth) | (xwest > xeast):
-        print('invalid bounding box')
-
-    # run R script and save output as random guid
-    print('invoking subsetting algorithm')
-
-    cmd = ['Rscript',
-           'subset_domain.R',
-           uid,
-           str(ysouth),
-           str(ynorth),
-           str(xwest),
-           str(xeast)]
-    print(' '.join(cmd))
-    p = subprocess.Popen(cmd,
-                         cwd=os.path.join(os.getcwd(),'r-subsetting'),
-                         stdout=subprocess.PIPE,
-                         stderr=subprocess.STDOUT)
-
-    for line in iter(p.stdout.readline, b''):
-        l = line.decode('utf-8').rstrip()
-        if 'std' in l:
-            l = l.replace('std:', '\n')
-            stdout.append(l)
-    p.stdout.close()
-    return_code = p.wait()
-
-    if not return_code == 0:
-        response = dict(message =
-            'The process call "{}" returned with code {}, an error '
-            'occurred.'.format(list(cmd), return_code),
-                       status='error')
-    else:
-        fpath = os.path.join('/tmp', uid) 
-
-        # save the stdout from the subsetting 
-        with open(os.path.join(fpath, 'stdout.txt'), 'w') as f:
-            for line in stdout:
-                f.write(line)
-               
-        outname = '%s.tar.gz' % uid
-
-        # compress the results
-        outname = '%s.tar.gz' % uid
-        with tarfile.open('/tmp/'+outname,  "w:gz") as tar:
-            tar.add(fpath, arcname=os.path.basename(fpath))
-        print('removing dir')
-        shutil.rmtree(fpath)
-
-        response = dict(message='file created at: /tmp/data/%s' % outname,
-                        filepath='/data/%s' % outname,
-                        status='success')
-
-    return response
-
-
-def mute():
-    sys.stdout = open(os.devnull, 'w') 
+#def subset_by_bbox(uid, llat, llon, ulat, ulon):
+#
+#    # list object to store stdout info for debugging
+#    stdout = []
+#    stdout.append('UID: %s\n'
+#                  'llat (WGS84): %s\n'
+#                  'llon (WGS84): %s\n'
+#                  'ulat (WGS84): %s\n'
+#                  'ulon (WGS84): %s\n\n' %(uid, str(llat), str(llon), str(ulat), str(ulon)))
+#
+#    geofile = '/home/acastronova/www.nco.ncep.noaa.gov/pmb/codes/nwprod/nwm.v1.2.2/parm/domain/geo_em.d01_1km.nc'
+#    bbox = (float(llon),
+#            float(llat),
+#            float(ulon),
+#            float(ulat))
+#    coords = [(bbox[0], bbox[1]), # lower left
+#              (bbox[2], bbox[3]), # upper left
+#              (bbox[2], bbox[3]), # upper right
+#              (bbox[0], bbox[1])] # lower right
+#    transformed = transform.proj_to_coord(coords)
+#    xs = [t[0] for t in transformed]
+#    ys = [t[1] for t in transformed]
+#    ysouth= min(ys)
+#    ynorth= max(ys)
+#    xwest = min(xs)
+#    xeast=  max(xs)
+#    stdout.append('Transformed (WGS -> Lambert Conformal Conic Variant)\n'
+#                  'llat (LCC, m): %s\n'
+#                  'llon (LCC, m): %s\n'
+#                  'ulat (LCC, m): %s\n'
+#                  'ulon (LCC, m): %s\n\n' %(str(ysouth), str(xwest), str(ynorth), str(xeast)))
+#
+#    # check that bbox is valid
+#    print('validating bounding box')
+#    if (ysouth > ynorth) | (xwest > xeast):
+#        print('invalid bounding box')
+#
+#    # run R script and save output as random guid
+#    print('invoking subsetting algorithm')
+#
+#    cmd = ['Rscript',
+#           'subset_domain.R',
+#           uid,
+#           str(ysouth),
+#           str(ynorth),
+#           str(xwest),
+#           str(xeast)]
+#    print(' '.join(cmd))
+#    p = subprocess.Popen(cmd,
+#                         cwd=os.path.join(os.getcwd(),'r-subsetting'),
+#                         stdout=subprocess.PIPE,
+#                         stderr=subprocess.STDOUT)
+#
+#    for line in iter(p.stdout.readline, b''):
+#        l = line.decode('utf-8').rstrip()
+#        if 'std' in l:
+#            l = l.replace('std:', '\n')
+#            stdout.append(l)
+#    p.stdout.close()
+#    return_code = p.wait()
+#
+#    if not return_code == 0:
+#        response = dict(message =
+#            'The process call "{}" returned with code {}, an error '
+#            'occurred.'.format(list(cmd), return_code),
+#                       status='error')
+#    else:
+#        fpath = os.path.join('/tmp', uid) 
+#
+#        # save the stdout from the subsetting 
+#        with open(os.path.join(fpath, 'stdout.txt'), 'w') as f:
+#            for line in stdout:
+#                f.write(line)
+#               
+#        outname = '%s.tar.gz' % uid
+#
+#        # compress the results
+#        outname = '%s.tar.gz' % uid
+#        with tarfile.open('/tmp/'+outname,  "w:gz") as tar:
+#            tar.add(fpath, arcname=os.path.basename(fpath))
+#        print('removing dir')
+#        shutil.rmtree(fpath)
+#
+#        response = dict(message='file created at: /tmp/data/%s' % outname,
+#                        filepath='/data/%s' % outname,
+#                        status='success')
+#
+#    return response
+#
+#
+#def mute():
+#    sys.stdout = open(os.devnull, 'w') 
