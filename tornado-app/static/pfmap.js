2ca9840613a6147006b5261da338c9ba43097807
Tony Castronova <castronova.anthony@gmail.com>
2019-11-22 11:40:09 -0500

    parflow styling

--- tornado-app/static/pfmap.js
+++ tornado-app/static/pfmap.js
@@ -1,6 +1,9 @@
-// credits: https://github.com/turban/Leaflet.Mask
-//
-document.addEventListener("DOMContentLoaded", function(){
+/*
+ * Extra functions for the ParFlow Map
+ * This script uses code provided by https://github.com/turban/Leaflet.Mask
+*/
+
+$(window).bind("load", function() {
 L.Mask = L.Polygon.extend({
     options: {
         stroke: false,
@@ -38,18 +41,33 @@ latLngs.push(new L.LatLng(48.1551, -121.4645));
 L.mask(latLngs).addTo(Map.map);
 
 
+/*
+ * Add Menu Button
+ */
+var menu = L.easyButton('fa-2x fa-bars', function (){toggleMenu();},
+			{position: 'topright'}).addTo(Map.map);
+menu.button.style.width = '40px';
 
-//// transform geojson coordinates into an array of L.LatLng
-//var coordinates = usa.features[0].geometry.coordinates;
-////for(j=0; j<coordinates.length; j++) {
-//for(j=164; j<165; j++) {
-//    console.log(j +': '+ coordinates[j][0].length);
-//    var latLngs = [];
-//    for (i=0; i<coordinates[j][0].length; i++) {
-//        latLngs.push(new L.LatLng(coordinates[j][0][i][1], coordinates[j][0][i][0]));
-//    }
-//
-//   L.mask(latLngs).addTo(Map.map);
-//}
+/*
+ * Add Submit Button
+ */
+var btn = '<strong>SUBMIT</strong>';
+var submitbtn = L.easyButton(btn,
+			     function (){submit();},
+			     {position: 'bottomright'}).addTo(Map.map);
+submitbtn.button.style.width = '100px';
+
 
 }); 
+
+function toggleMenu() {
+    var accordion = document.querySelector('#accordion');
+    var panel = document.querySelector('#menu-panel');
+    accordion.MaterialExtAccordion.command( {action: 'toggle', target: panel} );
+}
+
+function submit() {
+
+    document.getElementById('form-submit').submit();
+
+}
