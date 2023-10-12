ea3bac61c3256c26ccd822dfce682939eefb8de4
Scott Black <sblack@cuahsi.org>
2023-10-12 17:34:19 -0600

    clean up and and json response models

--- app/api/app/routers/argo/router.py
+++ app/api/app/routers/argo/router.py
@@ -6,10 +6,17 @@ import argo_workflows
 from app.users import current_active_user
 from argo_workflows.api import workflow_service_api
 from fastapi import APIRouter, Depends, Query
-from minio import Minio
 
 from api.app.db import User
-from api.config import get_settings
+from api.config import get_minio_client, get_settings
+
+from .models import (
+    LogsResponseModel,
+    UrlResponseModel,
+    UserSubmissionsResponseModel,
+    WorkflowDep,
+    WorkflowSubmissionResponseModel,
+)
 
 router = APIRouter()
 
@@ -20,11 +27,6 @@ configuration.api_key['BearerToken'] = get_settings().argo_bearer_token
 
 api_client = argo_workflows.ApiClient(configuration)
 api_instance = workflow_service_api.WorkflowServiceApi(api_client)
-
-
-client = Minio(
-    get_settings().minio_api_url, access_key=get_settings().minio_access_key, secret_key=get_settings().minio_secret_key
-)
 
 
 def parameters(hucs: list, workflow_name=str(uuid.uuid4())):
@@ -39,15 +41,17 @@ def parameters(hucs: list, workflow_name=str(uuid.uuid4())):
     }
 
 
-@router.get('/submit')
-async def submit(hucs: Annotated[list[str] | None, Query()], user: User = Depends(current_active_user)):
-    workflows_name = str(uuid.uuid4())
+@router.get('/submit/parflow')
+async def submit_parflow(
+    hucs: Annotated[list[str] | None, Query()], user: User = Depends(current_active_user)
+) -> WorkflowSubmissionResponseModel:
+    workflow_id = str(uuid.uuid4())
     api_instance.submit_workflow(
-        namespace=get_settings().argo_namespace, body=parameters(hucs, workflows_name), _preload_content=False
+        namespace=get_settings().argo_namespace, body=parameters(hucs, workflow_id), _preload_content=False
     )
-    user.workflow_submissions.append(workflows_name)
+    user.workflow_submissions.append({"workflow_id": workflow_id})
     await user.save()
-    return workflows_name
+    return {"workflow_id": workflow_id}
 
 
 def parse_logs(api_response):
@@ -59,26 +63,26 @@ def parse_logs(api_response):
     return logs
 
 
-@router.get('/logs')
-async def logs(workflow_name: str, user: User = Depends(current_active_user)):
+@router.get('/logs/{workflow_id}', description="logs for a workflow")
+async def logs(workflow_params: WorkflowDep) -> LogsResponseModel:
     api_response = api_instance.workflow_logs(
         namespace=get_settings().argo_namespace,
-        name=workflow_name,
+        name=workflow_params.workflow_id,
         _check_return_type=True,
         log_options_insecure_skip_tls_verify_backend=True,
         _check_input_type=True,
         log_options_container="main",
         _preload_content=False,
     )
-    return parse_logs(api_response)
+    return {"logs": parse_logs(api_response)}
 
 
-@router.get('/url')
-async def signed_url_minio(workflow_name: str, user: User = Depends(current_active_user)):
-    url = client.presigned_get_object("subsetter-outputs", f"parflow/{workflow_name}/subset")
-    return url
+@router.get('/url/{workflow_id}', description="Create a download url")
+async def signed_url_minio(workflow_params: WorkflowDep) -> UrlResponseModel:
+    url = get_minio_client().presigned_get_object("subsetter-outputs", f"parflow/{workflow_params.workflow_id}/subset")
+    return {'url': url}
 
 
 @router.get('/submissions')
-async def submissions(user: User = Depends(current_active_user)):
-    return user.workflow_submissions
+async def submissions(user: User = Depends(current_active_user)) -> UserSubmissionsResponseModel:
+    return {"submissions": user.workflow_submissions}
