acb2f854d9623b03ac4ca93606ed58507fdf2f83
Devin Cowan <dcowan@cuahsi.org>
2023-10-18 15:38:46 -0400

    add constants

--- app/frontend/src/constants.js
+++ app/frontend/src/constants.js
@@ -1,11 +1,11 @@
-export const APP_NAME = process.env.VUE_APP_NAME || "";
-export const APP_URL = process.env.VUE_APP_URL || "";
-export const LOGIN_URL = process.env.VUE_APP_LOGIN_URL || "";
-export const CLIENT_ID = process.env.OAUTH2_CLIENT_ID || "";
+export const APP_NAME = import.meta.env.VITE_APP_NAME || "";
+export const APP_URL = import.meta.env.VITE_APP_URL || "";
+export const LOGIN_URL = import.meta.env.VITE_APP_LOGIN_URL || "";
+export const CLIENT_ID = import.meta.env.OAUTH2_CLIENT_ID || "";
 
-export const API_BASE = process.env.VUE_APP_API_URL || "";
+export const API_BASE = import.meta.env.VITE_APP_API_URL || "";
 export const ENDPOINTS = {
-  redoc: `${API_BASE}/redoc`,
+  openapi: `${API_BASE}/openapi.json`,
   authCuahsiAuthorize: `${API_BASE}/auth/cuahsi/authorize`,
   authCuahsiCallback: `${API_BASE}/auth/cuahsi/callback`,
   submit: `${API_BASE}/submit`,
--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vuetify(),
   ],
+  envDir: "../",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
