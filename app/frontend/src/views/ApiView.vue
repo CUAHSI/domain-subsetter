1fddecc015d9a5f43b802f3851254dbeb99cc1dd
Devin Cowan <dcowan@cuahsi.org>
2023-10-18 20:33:15 -0400

    theme provider light for swagger

--- app/frontend/src/views/ApiView.vue
+++ app/frontend/src/views/ApiView.vue
@@ -1,15 +1,13 @@
 <template>
-  <v-theme-provider theme="light">
+  <v-theme-provider theme="light" with-background>
     <div class="swagger" id="swagger"></div>
   </v-theme-provider>
 </template>
 
 <script setup>
-import { ENDPOINTS } from "@/constants.js";
 import { onMounted } from 'vue'
 import SwaggerUI from 'swagger-ui';
 import 'swagger-ui/dist/swagger-ui.css';
-// const spec = import.meta.glob('@/../../spec/openapi.json')
 import json from '@/../../spec/openapi.json'
 onMounted(() => {
   SwaggerUI({
