083caedd4ec6ecc1826814baf91e0573149836d0
Devin Cowan <dcowan@cuahsi.org>
2023-10-30 14:31:18 -0400

    origins

--- app/api/main.py
+++ app/api/main.py
@@ -18,7 +18,7 @@ origins = json.loads(os.environ['ALLOW_ORIGINS'])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=[origins],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
