68a8dc51a04961f984d3f495a46fb3caf92b8c02
Devin Cowan <dcowan@cuahsi.org>
2023-11-14 11:23:42 -0500

    vite src

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   envDir: "../",
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('src', import.meta.url))
     }
   }
 })
