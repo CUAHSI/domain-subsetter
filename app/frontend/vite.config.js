9e41bfa26bb50cda151b52726f5b835e9c3d611a
Devin Cowan <dcowan@cuahsi.org>
2023-11-14 11:36:56 -0500

    resolve aliases for vite 3

--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -1,7 +1,6 @@
-import { fileURLToPath, URL } from 'url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 import vuetify from 'vite-plugin-vuetify'
 
 // https://vitejs.dev/config/
@@ -12,8 +11,12 @@ export default defineConfig({
   ],
   envDir: "../",
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('src', import.meta.url))
-    }
-  }
-})
+    alias: [
+      { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: '@assets', replacement: fileURLToPath(new URL('./src/shared/assets', import.meta.url)) },
+      { find: '@cmp', replacement: fileURLToPath(new URL('./src/shared/cmp', import.meta.url)) },
+      { find: '@stores', replacement: fileURLToPath(new URL('./src/shared/stores', import.meta.url)) },
+      { find: '@use', replacement: fileURLToPath(new URL('./src/shared/use', import.meta.url)) },
+    ],
+  },
+});
