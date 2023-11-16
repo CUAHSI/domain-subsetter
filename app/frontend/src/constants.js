74d44398920a3a288de90f69eb8b8ddba4bb9365
Devin Cowan <dcowan@cuahsi.org>
2023-11-16 08:43:34 -0500

    frontend use JWT instead of cookie

--- app/frontend/src/constants.js
+++ app/frontend/src/constants.js
@@ -1,14 +1,14 @@
 export const APP_NAME = import.meta.env.VITE_APP_NAME || "";
 export const APP_URL = import.meta.env.VITE_APP_URL || "";
-export const CLIENT_ID = import.meta.env.OAUTH2_CLIENT_ID || "";
+export const APP_BASE = import.meta.env.VITE_APP_BASE || "";
+export const OAUTH2_REDIRECT_URL = import.meta.env.VITE_OAUTH2_REDIRECT_URL || "";
 
 export const API_BASE = import.meta.env.VITE_APP_API_URL || "";
 export const ENDPOINTS = {
   openapi: `${API_BASE}/openapi.json`,
-  authCuahsiAuthorize: `${API_BASE}/auth/cookie/authorize`,
-  authCuahsiCallback: `${API_BASE}/auth/cookie/callback`,
+  authCuahsiAuthorize: `${API_BASE}/auth/front/authorize`,
+  authCuahsiCallback: `${API_BASE}/auth/front/callback`,
   authenticatedRoute: `${API_BASE}/authenticated-route`,
-  logout: `${API_BASE}/auth/cookie/logout`,
   userInfo: `${API_BASE}/users/me`,
   submit: `${API_BASE}/submit`,
   download: `${API_BASE}/url`,
