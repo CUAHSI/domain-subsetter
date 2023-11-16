2e123607ecf38d5e1198c469e734f81820d8fc17
Devin Cowan <dcowan@cuahsi.org>
2023-11-16 08:43:34 -0500

    frontend use JWT instead of cookie

--- app/frontend/src/stores/auth.js
+++ app/frontend/src/stores/auth.js
@@ -3,17 +3,19 @@ import { useStorage } from '@vueuse/core'
 
 export const useAuthStore = defineStore('auth', () => {
     const user = useStorage('user', {})
-    // const returnUrl = null
     const isLoggedIn = useStorage('isLoggedIn', false)
 
-    async function login(loginUser) {
-        this.isLoggedIn = true;
-        this.user = loginUser;
+    let storeToken = useStorage('storeToken', {})
+
+    async function login(token) {
+        // update pinia state
+        this.storeToken = token;
     }
 
     async function logout() {
+        this.user = {}
         this.isLoggedIn = false;
-        this.user = {};
+        this.storeToken = {};
     }
   
     return { isLoggedIn, user, login, logout }
