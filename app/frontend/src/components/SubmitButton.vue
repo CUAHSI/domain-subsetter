f9d2c4581824bd0e0836e8fda0c7db35b0c20e48
Devin Cowan <dcowan@cuahsi.org>
2023-11-30 11:46:56 -0500

    add proj4 for lcc

--- app/frontend/src/components/SubmitButton.vue
+++ app/frontend/src/components/SubmitButton.vue
@@ -7,6 +7,7 @@ import { useModelsStore } from '@/stores/models'
 import { ENDPOINTS } from '@/constants'
 import { useMapStore } from '@/stores/map'
 import { fetchWrapper } from '@/_helpers/fetchWrapper';
+import proj4 from 'proj4'
 
 const mapStore = useMapStore()
 const Map = mapStore.mapObject
@@ -34,7 +35,31 @@ async function submitHucs(selected_hucs, model) {
 }
 
 async function submitBbox(bbox, model) {
-  const [xmin, ymin, xmax, ymax] = bbox
+  let [xmin, ymin, xmax, ymax] = bbox
+  const lowerLeft = [xmin, ymin]
+  // const upperLeft = [xmin, ymax]
+  const upperRight = [xmax, ymax]
+  // const lowerRight = [xmax, ymin]
+  
+  // https://github.com/derhuerst/transform-coordinates
+  // import transformation from 'transform-coordinates'
+  // const transformation = require('transform-coordinates')
+  // https://epsg.io/3082
+  // const transform = transformation('EPSG:4326', '3082') // WGS 84 to LCC
+  
+  // var secondProjection = '+proj=lcc +lat_1=20 +lat_2=60 +lat_0=40 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs';
+  let secondProjection = '+proj=lcc +lat_1=30 +lat_2=60 +lat_0=40.0000076293945 +lon_0=-97 +x_0=0 +y_0=0 +a=6370000 +b=6370000 +units=m +no_defs'
+  
+  const lccLowerLeft = proj4(secondProjection, lowerLeft)
+  // const lccUpperLeft = proj4(secondProjection, upperLeft)
+  const lccUpperRight = proj4(secondProjection, upperRight)
+  // const lccLowerRight = proj4(secondProjection, lowerRight)
+
+  ymin = lccLowerLeft[1]
+  xmin = lccLowerLeft[0]
+  xmax = lccUpperRight[0]
+  ymax = lccUpperRight[1]
+
   const params = `y_south=${ymin}&y_north=${ymax}&x_west=${xmin}&x_east=${xmax}`
   alert(`Submitting bbox: ${bbox} for ${model} subsetting`)
 
