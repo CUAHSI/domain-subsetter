4d7160b807b6c09bbdf8343e7bdc358b5169e154
Devin Cowan <dcowan@cuahsi.org>
2023-12-08 11:16:09 -0500

    fix download

--- app/frontend/src/views/SubmissionsView.vue
+++ app/frontend/src/views/SubmissionsView.vue
@@ -6,7 +6,7 @@
       <div>startedAt: {{ submission.startedAt }}</div>
       <div>estimatedDuration: {{submission.estimatedDuration }}</div>
       <div>phase: {{ submission.phase }}</div>
-      <v-btn v-if="submission.phase == 'Succeeded'"><a :href="`${downloadEndpoint}/${submission.workflow_id}`" target="_blank">Download</a></v-btn>
+      <v-btn v-if="submission.phase == 'Succeeded'"><a @click="downloadArtifact(submission)" target="_blank">Download</a></v-btn>
       <v-btn><a :href="`${refreshEndpoint}/${submission.workflow_id}`" target="_blank">Refresh</a></v-btn>
   </v-card>
   <v-container>
@@ -17,11 +17,22 @@
 <script setup>
 import { useSubmissionsStore } from '@/stores/submissions'
 import { ENDPOINTS } from '@/constants'
+import { fetchWrapper } from '@/_helpers/fetchWrapper';
 
 const downloadEndpoint = ENDPOINTS.download
 const refreshEndpoint = ENDPOINTS.refresh
 const submissionStore = useSubmissionsStore();
 submissionStore.refreshWorkflows()
 submissionStore.getSubmissions()
+
+async function downloadArtifact(submission){
+  const downloadUrl = `${downloadEndpoint}/${submission.workflow_id}`
+  let response = await fetchWrapper.get(downloadUrl)
+  const link = document.createElement('a')
+  link.href = response.url
+  document.body.appendChild(link)
+  link.click()
+  document.body.removeChild(link);
+}
 
 </script>
