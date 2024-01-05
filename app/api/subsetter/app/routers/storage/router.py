305ac9cc47bc668899e3ea5edff377d8df852417
Scott Black <sblack@cuahsi.org>
2024-01-05 09:11:24 -0700

    add user bucket creation

--- app/api/subsetter/app/routers/storage/router.py
+++ app/api/subsetter/app/routers/storage/router.py
@@ -17,6 +17,14 @@ async def presigned_put_minio(workflow_params: WorkflowDep):
     return {'url': url}
 
 
+@router.post('/bucket/create')
+async def create_user_bucket(
+    user: User = Depends(current_active_user), description="Creates a bucket named with the username"
+):
+    if not get_minio_client().bucket_exists(user.username):
+        get_minio_client().make_bucket(user.username)
+
+
 @router.get('/upload/{user_name}/workflow/{workflow_id}')
 async def share_workflow_with_user(
     user_name: str, workflow_params: WorkflowDep, user: User = Depends(current_active_user)
