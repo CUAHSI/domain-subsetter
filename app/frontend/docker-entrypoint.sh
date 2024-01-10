465ea6425b547b2237ad937788f96a0c397e5b31
Devin Cowan <dcowan@cuahsi.org>
2024-01-09 20:54:54 -0500

    runtime env substitution

--- app/frontend/docker-entrypoint.sh
+++ app/frontend/docker-entrypoint.sh
@@ -1,6 +1,14 @@
-#!/bin/bash
+#!/bin/sh
+ROOT_DIR=/app
+# Replace env vars in files served by NGINX
+for file in $ROOT_DIR/assets/*.js $ROOT_DIR/index.html;
+do
+    echo "Processing $file ...";
+    # LC_ALL=C sed -i "" 's|VITE_APP_API_URL_PLACEHOLDER|'${VITE_APP_API_URL}'|g' $file
+    sed -i 's|VITE_APP_API_URL_PLACEHOLDER|'${VITE_APP_API_URL}'|g' $file
+    sed -i 's|VITE_APP_FULL_URL_PLACEHOLDER|'${VITE_APP_FULL_URL}'|g' $file
+    sed -i 's|VITE_APP_BASE_PLACEHOLDER|'${VITE_APP_BASE}'|g' $file
+done
 
-npm i @vue/cli-service
-npm install
-npm run build
-npm run serve -- --port 5003
+echo "Starting Nginx"
+nginx -g 'daemon off;'
--- app/frontend/src/constants.js
+++ app/frontend/src/constants.js
@@ -1,8 +1,7 @@
-export const APP_NAME = import.meta.env.VITE_APP_NAME || "";
-export const APP_URL = import.meta.env.VITE_APP_FULL_URL || "";
-export const APP_BASE = import.meta.env.VITE_APP_BASE || "";
+export const APP_URL = import.meta.env.VITE_APP_FULL_URL || "VITE_APP_FULL_URL_PLACEHOLDER";
+export const APP_BASE = import.meta.env.VITE_APP_BASE || "VITE_APP_BASE_PLACEHOLDER";
 
-export const API_BASE = import.meta.env.VITE_APP_API_URL || "";
+export const API_BASE = import.meta.env.VITE_APP_API_URL || "VITE_APP_API_URL_PLACEHOLDER";
 export const ENDPOINTS = {
   openapi: `${API_BASE}/openapi.json`,
   authCuahsiAuthorize: `${API_BASE}/auth/front/authorize`,
