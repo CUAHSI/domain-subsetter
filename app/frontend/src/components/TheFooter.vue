237d3a59e8b52a70f1f0855ddc60c131b5a9dd18
Devin Cowan <dcowan@cuahsi.org>
2024-09-04 13:33:20 -0400

    alter footer attribution

--- app/frontend/src/components/TheFooter.vue
+++ app/frontend/src/components/TheFooter.vue
@@ -1,9 +1,6 @@
 <template>
   <v-footer v-if="!$route.meta.hideNavigation" app color="navbar" class="d-flex flex-column">
     <div class="d-flex w-100 align-center">
-      <div class="text-center w-100">
-        {{ new Date().getFullYear() }} — <strong>hydroprocess_db</strong>
-      </div>
       <v-spacer></v-spacer>
       <v-card href="https://cuahsi.org" class="mx-auto" rel="noopener" target="_blank" subtitle="Powered by" min-width="
         16rem">
