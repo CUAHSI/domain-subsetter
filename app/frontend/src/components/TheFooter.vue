d6a73ca3888d0aff0e3dcf64b3b02b14bed5d122
Devin Cowan <dcowan@cuahsi.org>
2024-09-04 13:09:05 -0400

    light theme

--- app/frontend/src/components/TheFooter.vue
+++ app/frontend/src/components/TheFooter.vue
@@ -1,7 +1,6 @@
 <template>
   <v-footer v-if="!$route.meta.hideNavigation" app color="navbar" class="d-flex flex-column">
     <div class="d-flex w-100 align-center">
-      <ThemeButton/>
       <div class="text-center w-100">
         {{ new Date().getFullYear() }} — <strong>Subsetter</strong>
       </div>
@@ -10,9 +9,5 @@
     </div>
   </v-footer>
 </template>
-
-<script setup>
-import ThemeButton from './ThemeButton.vue';
-</script>
 
 <style lang="scss" scoped></style>
