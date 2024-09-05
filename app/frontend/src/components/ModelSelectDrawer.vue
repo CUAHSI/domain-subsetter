83e2f96b5855c6ad9c375e03a64b1f61d2d933ff
Devin Cowan <dcowan@cuahsi.org>
2024-09-05 09:29:27 -0400

    feat: Update ModelSelectDrawer to support nested models
    This commit modifies the ModelSelectDrawer component to support nested models. It introduces a new data structure in the models store that allows models to have parent-child relationships. The component now displays the parent models as slide group items, and when a parent model is selected, it expands to show its child models in an expansion panel. The user can select a child model to continue with the selection process. This enhancement improves the user experience by providing a more intuitive and organized way to select models.

--- app/frontend/src/components/ModelSelectDrawer.vue
+++ app/frontend/src/components/ModelSelectDrawer.vue
@@ -2,17 +2,16 @@
   <v-navigation-drawer location="left" width="auto" :model-value="props.show" temporary @update:modelValue="$emit('toggle')">
     <v-btn v-if="!props.show" size="large" color="primary" class="ma-0 pa-2 drawer-handle" @click="$emit('toggle')">
       <v-icon :icon="mdiGlobeModel"></v-icon>
-      <span v-if="isModelSelected">{{ modelsStore.selectedModel.shortName }}</span>
+      <span v-if="modelsStore.selectedModel">{{ modelsStore.selectedModel.shortName }}</span>
       <span v-else>Select Model</span>
       </v-btn>
     <v-sheet class="mx-auto" elevation="8" :width="mdAndDown ? '100vw' : '30vw'">
       <h2 class="ma-2 text-center">Model Selector</h2>
-      <v-slide-group v-model="modelsStore.selectedModel.value" class="pa-4" selected-class="bg-primary" show-arrows>
-        <v-slide-group-item v-for="hydroModel in models" :key="hydroModel.id"
-          v-slot="{ isSelected, toggle, selectedClass }">
-          <v-card color="grey-lighten-1" :class="['ma-4', selectedClass]" height="200" width="100"
-            @click="selectModel(hydroModel, toggle)">
-            <v-icon v-if="isSelected" :icon="mdiCheckboxMarked"></v-icon>
+      <v-slide-group v-model="selectedParent" class="pa-4" selected-class="bg-primary" show-arrows>
+        <v-slide-group-item v-for="hydroModel in models" :key="hydroModel.id" :value="hydroModel"
+          v-slot="{ toggle, selectedClass }">
+          <v-card color="grey-lighten-1" :class="['ma-4', selectedClass]" height="150" width="130"
+            @click="selectParent(hydroModel, toggle)">
             <div class="d-flex fill-height align-center justify-center">
               <v-scale-transition>
                 <div class="pa-4">
@@ -25,12 +24,21 @@
       </v-slide-group>
       <v-divider></v-divider>
       <v-expand-transition>
-        <v-sheet v-if="isModelSelected" height="200">
+        <v-expansion-panels variant="inset" class="my-4" expand v-model="panel">
+          <v-expansion-panel v-for="childModel in selectedParent.children" :key="childModel.shortName">
+            <v-expansion-panel-title><h3>{{childModel.name }}</h3></v-expansion-panel-title>
+            <v-expansion-panel-text>
+              <p>{{ childModel.description }}</p>
+              <v-btn color="primary" @click="selectModel(childModel)" :disabled="isDisabled(childModel)">Continue with {{ childModel.name }}</v-btn>
+            </v-expansion-panel-text>
+          </v-expansion-panel>
+          </v-expansion-panels>
+        <v-sheet v-if="modelsStore.selectedModel" height="200">
           <div class="d-flex fill-height align-center justify-center pa-4">
             <h3 class="text-h6">{{ modelsStore.selectedModel.description }}</h3>
           </div>
           <section class="text-right">
-            <v-btn v-if="isModelSelected" class="ma-2" color="primary" @click="$emit('toggle')">
+            <v-btn v-if="modelsStore.selectedModel" class="ma-2" color="primary" @click="$emit('toggle')">
               <span>Continue with {{ modelsStore.selectedModel.name }}</span>
               <v-icon class="ma-2" :icon="mdiArrowRightBoldBoxOutline"></v-icon>
             </v-btn>
@@ -44,24 +52,34 @@
 <script setup>
 import { useDisplay } from 'vuetify'
 import { useModelsStore } from '@/stores/models'
-import { computed } from 'vue';
-import { mdiGlobeModel, mdiCheckboxMarked, mdiArrowRightBoldBoxOutline } from '@mdi/js'
+import { ref } from 'vue';
+import { mdiGlobeModel, mdiArrowRightBoldBoxOutline } from '@mdi/js'
 
 const props = defineProps(['show'])
-defineEmits(['selectModel', 'toggle'])
+const emit = defineEmits(['selectModel', 'toggle'])
 
 const modelsStore = useModelsStore();
 const models = modelsStore.models
+const selectedParent = ref({})
+const panel = ref([0])
 
-function selectModel(model, toggle) {
+
+function selectModel(model) {
   // modelsStore.selectedModel = model
   modelsStore.updateModel(model)
+  emit('toggle')
+}
+
+function selectParent(parent, toggle) {
+  // modelsStore.selectedModel = model
+  selectedParent.value = parent
+  panel.value = [0]
   toggle()
 }
 
-const isModelSelected = computed(() => {
-  return modelsStore.selectedModel.value != null
-})
+function isDisabled(model) {
+  return model.shortName === 'nextgen1' || model.shortName === 'nwm3'
+}
 
 const { mdAndDown } = useDisplay()
 </script>
